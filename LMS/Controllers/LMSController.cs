3ad0f147b1f6448c2c45a8a8efafb685cab88f2e
TuuRanger <tuuranger@hotmail.com>
2016-04-26 10:57:15 +0700

    aa

---
+++ LMS/Controllers/LMSController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LMS.Controllers
+{
+    public class LMSController : Controller
+    {
+        //
+        // GET: /LMS/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Booking ()
+        {
+            return View();
+        }
+
+        public ActionResult BookingDetail()
+        {
+            return View();
+        }
+
+        public ActionResult Queue()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+
+        }
+
+        public ActionResult QueueDetail()
+        {
+            return View();
+        }
+    }
+}
